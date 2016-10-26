ecbbf174e2a626ac361756eee29c2b0908862802
zhiwei wang <dreamforday@sina.com>
2016-10-26 18:14:45 +0800

    初始化LFMBProgressHUDKit

---
+++ LFMBProgressHUDKit/LFUIView+MBProgressHUD.h
@@ -0,0 +1,92 @@
+//
+//  UIView+MBProgressHUD.h
+//  LFMBProgressHUDDemo
+//
+//  Created by WangZhiWei on 16/5/26.
+//  Copyright © 2016年 youku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class LFMBProgressHUD;
+
+@interface UIView (LFMBProgressHUDAdditions)
+
+
+/*!
+ 	@method
+ 	@abstract   显示转菊花
+ 	@param 	animated 是否支持动画
+ */
+- (void)lf_showHUDAnimated:(BOOL)animated;
+
+
+/**
+
+ @param animated 是否支持动画
+ */
+- (void)lf_hideHUDAnimated:(BOOL)animated;
+
+
+
+/*!
+ 	@method
+ 	@abstract   提示框,屏幕中间位置，支持多行 默认1.2秒消失
+  	@param 	message 	消息内容
+ 	@param 	animated    是否支持动画
+ */
+- (void)lf_showHUD:(NSString *)message animated:(BOOL)animated ;
+
+/*!
+ 	@method
+ 	@abstract   可以设定时间的提示框,屏幕中间位置，支持多行
+  	@param 	message 	消息内容
+ 	@param 	animated 是否支持动画
+ 	@param 	time 	延迟时间
+ */
+- (LFMBProgressHUD *)lf_showHUD:(NSString *)message
+                       animated:(BOOL)animated
+                      hideAfter:(NSTimeInterval)time;
+
+
+
+/**
+ *  老版本解决键盘弹出时候挡住提示框的一种办法（其实只是由多行变为只支持单行）
+ *
+ *  @param message 消息内容
+ *  @param offsetY 坐标位置
+ *  @param animated 是否支持动画
+ */
+
+- (void)popupMessage:(NSString*)message offsetY:(CGFloat)offsetY animated:(BOOL)animated;
+
+
+/*!
+ @method
+ @param 	message 	消息内容
+ @param     image  图片资源
+ @param 	animated 是否支持动画
+ @param 	time 	延迟时间
+ */
+- (void)lf_showHUD:(NSString *)message image:(UIImage *)image animated:(BOOL)animated hideAfter:(NSTimeInterval)time;
+
+
+/*!
+ 	@method
+ 	@discussion	删除
+ 	@param 	animated 删除
+ */
+- (void)lf_removeAllHUDAnimated:(BOOL)animated;
+
+
+
+#pragma mark --过期方法
+- (void)lf_showHUDAnimated:(BOOL)animated message:(NSString *)message __attribute__((deprecated("Use lf_showHUD: animated: instead.")));
+
+- (LFMBProgressHUD *)lf_showHUDAnimated:(BOOL)animated message:(NSString *)message
+                              dalayTime:(NSTimeInterval)time __attribute__((deprecated("Use - (LFMBProgressHUD *)lf_showHUD: animated: dalay: instead.")));
+
+
+
+
+
+@end
---
+++ LFMBProgressHUDKit/LFUIView+MBProgressHUD.m
@@ -0,0 +1,116 @@
+//
+//  UIView+MBProgressHUD.m
+//  LFMBProgressHUDDemo
+//
+//  Created by WangZhiWei on 16/5/26.
+//  Copyright © 2016年 youku. All rights reserved.
+//
+
+#import "LFUIView+MBProgressHUD.h"
+#import "LFMBProgressHUD.h"
+
+@implementation UIView (LFMBProgressHUDAdditions)
+
+
+- (void)lf_showHUDAnimated:(BOOL)animated
+{
+    [self lf_hideHUDAnimated:animated];
+    
+    [LFMBProgressHUD showHUDAddedTo:self animated:animated];
+
+}
+
+- (void)lf_hideHUDAnimated:(BOOL)animated
+{
+    [LFMBProgressHUD hideHUDForView:self animated:animated];
+}
+
+
+- (void)lf_showHUD:(NSString *)message animated:(BOOL)animated
+{
+    [self showTimedHUD:message animated:animated hideAfter:1.2];
+}
+
+- (void)showTimedHUD:(NSString *)message animated:(BOOL)animated hideAfter:(NSTimeInterval)time {
+    
+    [self lf_hideHUDAnimated:animated];
+    if (message.length == 0) return;
+    
+    LFMBProgressHUD *hud = [LFMBProgressHUD showHUDAddedTo:self animated:animated];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.label.text = message;
+    [hud hideAnimated:animated afterDelay:time];
+}
+
+- (LFMBProgressHUD *)lf_showHUD:(NSString *)message
+                       animated:(BOOL)animated
+                      hideAfter:(NSTimeInterval)time
+{
+    [self lf_hideHUDAnimated:animated];
+    if (message.length == 0) return nil;
+    
+    LFMBProgressHUD *hud = [LFMBProgressHUD showHUDAddedTo:self animated:animated];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.label.text = message;
+    [hud hideAnimated:animated afterDelay:time];
+    return hud;
+}
+
+
+
+- (void)popupMessage:(NSString*)message offsetY:(CGFloat)offsetY animated:(BOOL)animated {
+    [self lf_removeAllHUDAnimated:animated];
+    if (message.length == 0) return;
+
+    LFMBProgressHUD *hud = [LFMBProgressHUD showHUDAddedTo:self animated:YES];
+    hud.label.text = message;
+    hud.mode = MBProgressHUDModeText;
+    hud.bezelView.layer.cornerRadius = 2.5f;
+    hud.margin = 10.f;
+    hud.offset = CGPointMake(0, 0);
+    hud.removeFromSuperViewOnHide = YES;
+    hud.userInteractionEnabled = NO;
+    hud.offset = CGPointMake(hud.offset.x, offsetY);
+    
+    [hud hideAnimated:animated afterDelay:2.0];
+}
+
+
+
+- (void)lf_showHUD:(NSString *)message image:(UIImage *)image animated:(BOOL)animated hideAfter:(NSTimeInterval)time;
+{
+    [self lf_removeAllHUDAnimated:animated];
+    if (message.length == 0) return;
+    if (image == nil) return;
+    
+    LFMBProgressHUD *hud = [[LFMBProgressHUD alloc] initWithView:self];
+    [self addSubview:hud];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.removeFromSuperViewOnHide = YES;
+    hud.square = YES;
+    hud.customView = [[UIImageView alloc] initWithImage:image];
+    hud.label.text = message;
+    hud.label.font = [UIFont boldSystemFontOfSize:10.0f];
+    [hud showAnimated:animated];
+    [hud hideAnimated:animated afterDelay:time];
+}
+
+- (void)lf_removeAllHUDAnimated:(BOOL)animated
+{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    [LFMBProgressHUD hideAllHUDsForView:self animated:animated];
+#pragma clang diagnostic pop
+}
+
+
+- (void)lf_showHUDAnimated:(BOOL)animated message:(NSString *)message{
+    
+    [self lf_showHUD:message animated:animated];
+}
+
+- (LFMBProgressHUD *)lf_showHUDAnimated:(BOOL)animated message:(NSString *)message
+                              dalayTime:(NSTimeInterval)time{
+    return [self lf_showHUD:message animated:animated hideAfter:time];
+}
+@end
